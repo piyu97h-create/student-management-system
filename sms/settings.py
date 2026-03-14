dae564fdbcfbc550b0b9f5770b56ded1db7a0941
piyu97h-create <piyu97h@gmail.com>
2026-03-14 15:03:51 +0530

    static file

--- sms/settings.py
+++ sms/settings.py
@@ -117,3 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    "demo_static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
